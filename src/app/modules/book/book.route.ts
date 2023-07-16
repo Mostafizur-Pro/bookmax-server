a606ff2ee3fc7e2f65a068fddbb6fb8904cdd895
Mostafizur-Pro <mostafizur0195@gmail.com>
2023-07-16 17:26:52 +0600

    successfully book add

--- src/app/modules/book/book.route.ts
+++ src/app/modules/book/book.route.ts
@@ -2,7 +2,7 @@ import express from "express";
 import bookController from "./book.controller";
 
 const router = express.Router();
-router.post("/createbook", bookController.createBook);
+router.post("/create-book", bookController.createBook);
 router.get("", bookController.getAllBook);
 
 export const BookRoute = router;
